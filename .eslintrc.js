ab36dcdc88f39b5ba5498bc6169734ada2b23ac8
Drew Harper <druharper@gmail.com>
2021-02-16 13:48:58 -0600

    eslint update

--- .eslintrc.js
+++ .eslintrc.js
@@ -9,11 +9,28 @@ module.exports = {
   },
   rules: {
     "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
-    "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off",
-    // allow paren-less arrow functions
-    "arrow-parens": 0,
-    // allow async-await
-    "generator-star-spacing": 0,
+    // "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off",
     // allow debugger during development
+    // allow paren-less arrow functions
+    'arrow-parens': 1,
+    // INDENT DEFAULT AT 2 SPACES
+    'vue/html-quotes': ['error', 'single', { avoidEscape: false }],
+    // indent: ['error', 2],
+    // 'vue/script-indent': ['error', 2, { baseIndent: 1 }],
+    'generator-star-spacing': 0,
+    quotes: ['error', 'single', 'avoid-escape'],
+    // quotes: ['error', 'single'],
+    semi: ['error', 'never'],
+    'prettier/prettier': ['error', { singleQuote: true, parser: 'flow' }],
+    'no-debugger': process.env.NODE_ENV === 'production' ? 2 : 0,
+    // 'space-before-function-paren': ['error', 'never'],
+    'space-before-function-paren': [
+      'error',
+      {
+        anonymous: 'never',
+        named: 'never',
+        asyncArrow: 'ignore',
+      },
+    ],
   },
 }
